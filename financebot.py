237fb1e84d2dbe6a72e3884fe31939a4918ec069
Bruno Salgado <bsalgado@stevens.edu>
2020-04-08 19:46:52 -0400

    Updated .gitignore and now utilizing slack events API

--- financebot.py
+++ financebot.py
@@ -1,11 +1,16 @@
 import os
-import slack
+from slackeventsapi import SlackEventAdapter
 
-slack_token = os.environ.get("SLACK_BOT_TOKEN")
+slack_token = os.environ.get("SLACK_SIGNING_SECRET")
 print(slack_token)
-client = slack.WebClient(token=slack_token)
 
-client.chat_postMessage(
-  channel="#slackfinancebot",
-  text="Hello from your app! :tada:"
-)
+slack_events_adapter = SlackEventAdapter(slack_token, endpoint="/slack/events")
+
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    message = event_data["event"]
+    if message.get("subtype") is None and "get" in message.get('text'):
+        channel = message["channel"]
+        message = ""
+
+slack_events_adapter.start(port=3000)
