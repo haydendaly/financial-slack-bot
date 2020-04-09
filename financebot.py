34761f11c4b3488e3c5bc9292d01f43d92b80362
Scott Murray <smurray1@stevens.edu>
2020-04-09 13:34:18 -0400

    integrated avrequest router into slackbot

--- financebot.py
+++ financebot.py
@@ -18,23 +18,13 @@ def handle_message(event_data):
     channel = message["channel"]
     if message.get("subtype") is None and bot_id in message.get('text'):
         text = message.get('text').lower()
-        if "get quote" in text:
-            ticker = text.split(" ")[3].upper()
-            response = avrequest.getQuote(ticker)
-        elif "get wma" in text:
-            ticker = text.split(" ")[3].upper()
-            response = avrequest.getWMA(ticker)
-        elif "get exchange rate" in text:
-            currency1 = text.split(" ")[4].upper()
-            currency2 = text.split(" ")[5].upper()
-            response = avrequest.getExchangeRate(currency1, currency2)
-        elif "help" in text:
+		if "help" in text:
             response = "Here is a list of things you can ask me:\n"
             response += "get quote <stock_ticker> (e.g. get quote GOOG)\n"
             response += "get wma <stock_ticker> (e.g. get wma AAPL)\n"
             response += "get exchange rate <currency1> <currency2> (e.g. get exchange rate USD CAD)"
         else:
-            response = "Sorry, I don't know what you're asking. Try asking me 'help' to see all the things you can ask me!"
+            response = avrequest.router(text)
         client.chat_postMessage(
             channel=channel,
             text=response
