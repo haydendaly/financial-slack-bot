e3e711641f040084e01ac0ee46e992f7fca67d47
Scott Murray <smurray1@stevens.edu>
2020-04-19 21:40:27 -0400

    getOBV, getRSI, getSTOCH

--- avrequest.py
+++ avrequest.py
@@ -38,6 +38,51 @@ def getWMA(ticker):
 		break
 	result = bestMatch["1. symbol"] + " (" + bestMatch["2. name"] + ") Weekly Moving Average is $" + wmaValue + " for the week of " + timeWMA
 	return result
+	
+def getOBV(ticker):
+	bestMatch = search(ticker)
+	obv = requests.get(url + "OBV&symbol=" + bestMatch["1. symbol"] + "&interval=weekly&apikey=" + apikey).json()["Technical Analysis: OBV"]
+	result = ""
+	i = 0
+	for key in obv:
+		result = result + key + ": " + obv[key]["OBV"] + "\n"
+		i += 1
+		if i >= 5: 
+			break
+	
+	return result
+	
+def getRSI(ticker, series):
+	valid_series = ["closed", "open", "high", "low"]
+	
+	if series not in valid_series:
+		return "Invalid series type " + series
+	
+	bestMatch = search(ticker)
+	rsi = requests.get(url + "RSI&symbol=" + bestMatch["1. symbol"] + "&interval=weekly&time_period=60&series_type=" + series + "&apikey=" + apikey).json()
+	print(rsi)
+	result = ""
+	i = 0
+	for key in rsi["Technical Analysis: RSI"]:
+		result = result + key + ": " + rsi["Technical Analysis: RSI"][key]["RSI"] + "\n"
+		i += 1
+		if i >= 5: 
+			break
+	
+	return result
+	
+def getSTOCH(ticker):
+	bestMatch = search(ticker)
+	stoch = requests.get(url + "STOCH&symbol=" + bestMatch["1. symbol"] + "&interval=weekly&apikey=" + apikey).json()["Technical Analysis: STOCH"]
+	result = ""
+	i = 0
+	for key in stoch:
+		result = result + key + ":\n   SlowD: " + stoch[key]["SlowD"] + "\n   SlowK: " + stoch[key]["SlowK"] + "\n"
+		i += 1
+		if i >= 5: 
+			break
+	
+	return result
 
 def router(string):
 	words = string.split(" ")
@@ -47,8 +92,14 @@ def router(string):
 		return getExchangeRate(words[1].split("=")[1], words[2].split("=")[1])
 	elif words[0] == "getWMA":
 		return getWMA(words[1].split("=")[1])
+	elif words[0] == "getOBV":
+		return getOBV(words[1].split("=")[1])
+	elif words[0] == "getRSI":
+		return getRSI(words[1].split("=")[1], words[2].split("=")[1])
+	elif words[0] == "getSTOCH":
+		return getSTOCH(words[1].split("=")[1])
 	else:
 		return False
 
 if __name__ == "__main__":
-	print(router("getWMA -ticker=IBM"))
+	print(router("getRSI -ticker=IBM -series=closed"))
